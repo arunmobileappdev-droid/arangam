e0baf8a528079bb032bbb78202fedf7397e2fc33
Arunkumar <arun@nexdha.com>
2026-02-18 14:04:42 +0530

    Setup Setting.py

--- arangam_project/settings.py
+++ arangam_project/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -86,8 +87,9 @@ DATABASES = {
     #     'HOST': 'localhost',
     #     'PORT': '5432',
     # }
-     'default': dj_database_url.config(
-        default=os.environ.get('postgresql://arun_db_user:jhhT0NZ3dpnc00uOu0QoXiYslTufKo0W@dpg-d6am7engi27c73d64hn0-a/arun_db')
+    'default': dj_database_url.config(
+        conn_max_age=600,
+        ssl_require=True
     )
 }
 
@@ -126,7 +128,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
